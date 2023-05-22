022681822704d4deaa0c1d365d790c8bcc1db346
ynxgnk <nazar.iosdev@gmail.com>
2023-05-22 07:15:48 +0200

    Added SearchBar with query, outputting each character from search to console

--- Stocks/Controllers/WatchListViewController.swift
+++ Stocks/Controllers/WatchListViewController.swift
@@ -18,6 +18,26 @@ class WatchListViewController: UIViewController {
     private func setUpSearchController() { /* 54 */
         let resultVC = SearchResultsViewController() /* 56 */
         let searchVC = UISearchController(searchResultsController: resultVC) /* 57 */
+        searchVC.searchResultsUpdater = self /* 59 */
+        navigationItem.searchController = searchVC /* 58 */
     }
 
+}
+
+extension WatchListViewController: UISearchResultsUpdating { /* 60 */
+    func updateSearchResults(for searchController: UISearchController) { /* 61 */
+        guard let query = searchController.searchBar.text,
+              let resultsVC = searchController.searchResultsController as? SearchResultsViewController,
+              !query.trimmingCharacters(in: .whitespaces).isEmpty else { /* 62 */
+            return /* 63 */
+        }
+        
+        //Optimize to reduce number of searches for when user stops typing
+        
+        //Call API to search
+        
+        //Update results controller
+        
+        print(query) /* 64 */
+    }
 }
